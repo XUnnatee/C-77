feb2200cc25f9af9206d5b622cfcec000caee44d
XUnnatee <98157500+XUnnatee@users.noreply.github.com>
2022-08-14 11:06:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var title = "My first console project";
+console.log(title);
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+console.log(first_name , last_name);
+ var a = 15;
+ var b = 16;
+ var add = a + b;
+ console.log(add)
